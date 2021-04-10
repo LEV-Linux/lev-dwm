dcfd5c5e76bbf9d57e6939eb1001beecfd4dbec8
salahdin-ahmed <salah2112004@gmail.com>
2021-04-10 10:42:09 +0200

    Change shift view keybind

--- config.def.h
+++ config.def.h
@@ -122,8 +122,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY|Mod1Mask,              XK_l,      shiftview,      {.i = +1 } },
-	{ MODKEY|Mod1Mask,              XK_h,      shiftview,      {.i = -1 } },
+	{ MODKEY|ControlMask,           XK_l,      shiftview,      {.i = +1 } },
+	{ MODKEY|ControlMask,           XK_h,      shiftview,      {.i = -1 } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
